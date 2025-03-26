3bd2d2a51840816b1ad28fa8307559fae09297c1
Challa Pavan Bhaskar <107108400+pavanbhaskardev@users.noreply.github.com>
2025-03-26 11:29:03 +0530

    Removed the explicit error

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   console.log("👋 hello there")
-  console.log(`Env: NEXT_PUBLIC_TEXT=${NEXT_PUBLIC_TEXT}`)
-
   return <Component {...pageProps} />
 }
 
